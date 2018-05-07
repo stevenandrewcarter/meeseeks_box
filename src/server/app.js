b0dd337b6c302f6f23fbda39e8318913e58cc2d7
Steven Carter <stonemason@gmail.com>
2018-05-07 21:36:38 +0200

    * Added back www

--- src/server/app.js
+++ src/server/app.js
@@ -5,8 +5,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
 const meeseeks = require('./routes/meeseeks');
 
 const app = express();
