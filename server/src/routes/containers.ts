df30152c65614bd173915d717ec550202e99cee5
Steven Carter <stonemason@gmail.com>
2022-10-08 14:46:15 +0800

    Updated the versions

--- server/src/routes/containers.ts
+++ server/src/routes/containers.ts
@@ -1,11 +1,11 @@
-import {Router, Request, Response} from 'express';
+import {Router, Request, Response, NextFunction} from 'express';
 import {Logger} from '../utils/logger';
 import Docker from 'dockerode';
 
 export const containersRoute = Router();
 const docker = new Docker();
 
-containersRoute.get('/containers', async (_req: Request, res: Response, next) => {
+containersRoute.get('/containers', async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const containers = await docker.listContainers({all: true});
     res.json(containers);
@@ -14,25 +14,25 @@ containersRoute.get('/containers', async (_req: Request, res: Response, next) =>
   }
 });
 
-containersRoute.post('/containers', async (_req: Request, res: Response, next) => {
+containersRoute.post('/containers', async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const container = await docker.createContainer({Image: 'alpine', Cmd: ['/bin/sh'], name: 'alpine-test'});
     await container.start();
     res.status(201).json(container);
   } catch (err) {
     next(err);
-    // res.status(400).json(err);
   }
 });
 
-containersRoute.delete('/containers/:containerId', async (req: Request, res: Response) => {
+containersRoute.delete('/containers/:containerId', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const {containerId} = req.params;
     const container = docker.getContainer(containerId);
     Logger.debug(container);
     // await container.stop();
     await container.remove();
+    res.status(200);
   } catch (err) {
-    res.status(400).json(err);
+    next(err);
   }
 });
